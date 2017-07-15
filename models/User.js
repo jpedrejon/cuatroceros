39e7470d359245f6fe0e443ae45a26e2ecc3fd81
Jorge Pedrejon <jgpedrejon@gmail.com>
2017-07-16 01:24:13 +0200

    saturday

---
+++ models/User.js
@@ -0,0 +1,22 @@
+// models/user.js
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  name: String,
+  lastName: String,
+  email: String,
+  dateOfBrith: Date,
+  address: String,
+  location: Object,
+  role: String,
+  accessToken: String
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
